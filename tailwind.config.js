abb87f51cff193b4897340efc989c7490d74f6df
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-15 07:27:41 -0300

    chore(tailwind.config): added grid pattern and flag mode jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.tsx'],
+  safelist: [
+    {
+      pattern: /grid-cols-./,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {

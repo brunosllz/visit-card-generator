2c5eeb1a6972e8489518c70ede6fe836ddfa763a
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-15 13:08:44 -0300

    refactor(home): implement component form

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { GenerateCardForm } from '@/components/GenerateCardFom/Index'
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-900 w-full h-screen flex justify-center items-center">
+      <GenerateCardForm />
+    </div>
+  )
+}

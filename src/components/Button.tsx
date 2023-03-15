6b356cbf13e8b2f7b801b232681a42e8187cfd8e
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-15 09:33:25 -0300

    styles(button): added variant style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,32 @@
 import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react'
+import clsx from 'clsx'
 
 import { CircleNotch } from 'phosphor-react'
 
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   title: string
   children?: ReactNode
+  variant?: 'primary' | 'secondary'
   isLoading?: Boolean
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ title, children, isLoading, ...props }: ButtonProps, ref) => {
+  (
+    { title, children, isLoading, variant = 'primary', ...props }: ButtonProps,
+    ref,
+  ) => {
     return (
       <button
         ref={ref}
-        className="text-sm rounded-md py-4 flex h-12 gap-2 justify-center items-center w-full transition-color bg-green-700 hover:bg-green-600 disabled:bg-green-700 disabled:opacity-50 disabled:cursor-not-allowed "
+        className={clsx(
+          'text-sm rounded-md py-4 flex h-12 gap-2 justify-center items-center w-full min-w-[120px] transition-colors text-white',
+          {
+            'bg-green-600  hover:bg-green-500 disabled:bg-green-700 disabled:cursor-not-allowed':
+              variant === 'primary',
+
+            'bg-transparent hover:bg-gray-600': variant === 'secondary',
+          },
+        )}
         {...props}
       >
         {isLoading ? (

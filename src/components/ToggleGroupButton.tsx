8384aa13fe3647bf30599dc8ec8cf0e479913a48
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-16 09:18:09 -0300

    refactor(toggleGroupButton): changeded structure of component

--- src/components/ToggleGroupButton.tsx
+++ src/components/ToggleGroupButton.tsx
@@ -1,41 +1,62 @@
-import * as ToggleGroup from '@radix-ui/react-toggle-group'
 import { Control, FieldValues, Path, useController } from 'react-hook-form'
+
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+import { forwardRef, Ref } from 'react'
 
 interface ToggleGroupButtonProps<T extends FieldValues = FieldValues> {
   name: Path<T>
   control: Control<T>
 }
 
-export function ToggleGroupButton<T extends FieldValues = FieldValues>({
-  control,
-  name,
-}: ToggleGroupButtonProps<T>) {
-  const {
-    field: { value, onChange, ref },
-  } = useController({ name, control })
+export const ToggleGroupButton = forwardRef<
+  HTMLDivElement,
+  ToggleGroupButtonProps
+>(
+  <T extends FieldValues = FieldValues>(
+    { control, name }: ToggleGroupButtonProps<T>,
+    ref: Ref<HTMLDivElement> | undefined,
+  ) => {
+    const {
+      fieldState: { error },
+      field: { value, onChange },
+    } = useController({ name, control })
 
-  return (
-    <ToggleGroup.Root
-      type="single"
-      className="bg-zinc-900 flex items-center gap-1 rounded-md"
-      defaultValue="#FFFFFF"
-      value={value}
-      onValueChange={onChange}
-      ref={ref}
-    >
-      <ToggleGroup.Item
-        value="#000000"
-        className="p-2 w-full data-[state=on]:bg-zinc-500 rounded-md transition-colors"
-      >
-        Black
-      </ToggleGroup.Item>
-      <ToggleGroup.Item
-        value="#FFFFFF"
-        defaultChecked={true}
-        className="p-2 w-full  data-[state=on]:bg-zinc-500 rounded-md transition-colors"
-      >
-        White
-      </ToggleGroup.Item>
-    </ToggleGroup.Root>
-  )
-}
+    return (
+      <div className="flex flex-col gap-1" ref={ref}>
+        <label htmlFor={name} className="flex flex-col gap-2">
+          Text color
+          <ToggleGroup.Root
+            id={name}
+            type="single"
+            className="flex flex-col items-center gap-1 rounded-md overflow-hidden"
+            defaultValue="#FFFFFF"
+            value={value}
+            onValueChange={onChange}
+          >
+            <div className="flex w-full">
+              <ToggleGroup.Item
+                value="#000000"
+                className="p-2 w-full data-[state=on]:bg-zinc-500  transition-colors bg-zinc-900"
+              >
+                Black
+              </ToggleGroup.Item>
+              <ToggleGroup.Item
+                value="#FFFFFF"
+                defaultChecked={true}
+                className="p-2 w-full  data-[state=on]:bg-zinc-500  transition-colors bg-zinc-900"
+              >
+                White
+              </ToggleGroup.Item>
+            </div>
+          </ToggleGroup.Root>
+        </label>
+
+        {!!error && (
+          <span className="text-red-500 text-xs">{error.message}</span>
+        )}
+      </div>
+    )
+  },
+)
+
+ToggleGroupButton.displayName = 'ToggleGroupButton'

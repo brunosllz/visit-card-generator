86284eda7bfe4416fd7eb49e37602274045efb85
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-17 14:19:22 -0300

    chore(env): added dev and prod url

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,14 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  PRODUCTION_URL: z.string(),
+  DEVELOPMENT_URL: z.string(),
 })
 
 export const env = envSchema.parse({
   NODE_ENV: process.env.NODE_ENV,
+  PRODUCTION_URL: process.env.PRODUCTION_URL ?? '',
+  DEVELOPMENT_URL: process.env.DEVELOPMENT_URL ?? '',
 })
+
+console.log(process.env.PRODUCTION_URL)

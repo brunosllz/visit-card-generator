66c3998cdda0475fba9a266f8b7c666ec7586fcf
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-17 09:01:56 -0300

    chore(vitest): added alias path resolve

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -10,5 +11,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })

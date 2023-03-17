9753310529eb708dcf5d042aa38b0c01b6c97460
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-17 08:10:18 -0300

    feat(app): added default seo on the component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
+import { DefaultSeo } from 'next-seo'
 
 import '@/styles/globals.css'
 import 'react-toastify/dist/ReactToastify.css'
@@ -7,6 +8,15 @@ import 'react-toastify/dist/ReactToastify.css'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'pt_BR',
+          url: 'http://localhost:3000',
+          title: 'Generate your visit card fast and easy',
+          siteName: 'Visit Card Generator',
+        }}
+      />
       <Component {...pageProps} />
       <ToastContainer theme="dark" />
     </>

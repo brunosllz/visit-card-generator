2212799b88440f0b6f07f0e2e5effdc7f45b585e
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-17 13:21:24 -0300

    refactor(api): added prodution domain

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,8 @@ import { env } from '@/env'
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : '',
+  baseURL:
+    env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api'
+      : 'https://visit-card-generator-pi.vercel.app/api',
 })

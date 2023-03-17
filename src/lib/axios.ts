5c867aaf49837cb7eb8ab7644cfa0deb27a131e8
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-17 13:55:39 -0300

    fix(lib): fix base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,5 @@
-import { env } from '@/env'
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    env.NODE_ENV === 'development'
-      ? 'http://localhost:3000/api'
-      : 'https://visit-card-generator-pi.vercel.app/api',
+  baseURL: '/api',
 })

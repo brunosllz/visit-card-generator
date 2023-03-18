73f5d483c8806939d3842f05bbfe55597ac9d703
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-18 11:28:39 -0300

    testes: added multi step

---
+++ src/components/MultiStep.spec.tsx
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import { MultiStep } from './MultiStep'
+
+describe('Card preview component', () => {
+  it('Should be render correctly', () => {
+    const CURRENT_STEP = 1
+    const SIZE = 5
+
+    render(<MultiStep currentStep={1} size={5} />)
+
+    expect(screen.getByText(`${CURRENT_STEP} of ${SIZE}`)).toBeInTheDocument()
+  })
+})
